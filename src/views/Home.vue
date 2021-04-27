1358ef18173050fa1a92c7815dfa942b53fad1e1
Pablo Guzman <pguzman89@uabc.edu.mx>
2021-04-27 14:30:53 -0700

    The HomeView was changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,9 +15,9 @@
                         >
                           Iniciar sesión
                         </h1>
-                        <h4 class="text-center mt-4">
+                        <h3 class="text-center mt-4">
                           Tu información esta protegida con nosotros.
-                        </h4>
+                        </h3>
                         <v-form>
                           <v-text-field
                             v-model="email"
@@ -27,6 +27,7 @@
                             prepend-icon="mdi-email"
                             type="text"
                             color="green darken-2"
+                            :rules="requisito"
                           />
                           <v-text-field
                             id="password"
@@ -36,28 +37,31 @@
                             prepend-icon="mdi-lock"
                             type="password"
                             color="green darken-2"
+                            :rules="requisito"
                           />
                         </v-form>
                       </v-card-text>
                       <div class="text-center mt-5 mb-5">
-                        <v-btn rounded color="accent-3" dark
-                        @click="validarSesion">Iniciar</v-btn>
+                        <v-btn
+                          rounded
+                          color="accent-3"
+                          dark
+                          @click="validarSesion"
+                          >Iniciar</v-btn
+                        >
                       </div>
                     </v-col>
                     <v-col cols="12" md="4" class="green darken-2">
                       <v-card-text class="white--text mt-12">
                         <h1 class="text-center display-1">
-                          Bienvenido al sistema de administración de proyectos de vinculación!
+                          Administración de proyectos
+                          de vinculación
                         </h1>
-                        <h5 class="text-center">
-                          La información de tus alumnos con unos cuantos clicks.
-                          {{this.email +"uabc.edu.mx"}}
-                        </h5>
                         <v-img
                           v-bind:src="require('../assets/escudoUabc.png')"
-                          :aspect-ratio=".75"
-                          :width ="150"
-                          class="mt-4 ml-8 mb-5"
+                          :aspect-ratio="0.75"
+                          :width="180"
+                          class="mt-4 ml-16"
                         />
                       </v-card-text>
                     </v-col>
@@ -73,7 +77,6 @@
 </template>
 
 <script>
-const axios = require('axios').default;
 export default {
   data() {
     return {
@@ -82,29 +85,12 @@ export default {
       password: "",
       rules: {
         requisito: (value) => !!value || "Requerido",
-        correo: (value) => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          return pattern.test(value) || "Correo invalido.";
-        },
       },
     };
   },
   props: {
     source: String,
   },
-  methods: {
-    validarSesion : function(){
-      axios.post('https://llave.uabc.edu.mx/auth/login',{
-        username: "pguzman89",
-        password: "Pablo@GTFO1",
-      })
-      .then(function(response){
-        console.log("SUCCESS: " + response);
-      })
-      .catch(function(e){
-        console.log("ERROR: " + e);
-      })
-    }
-  },
+  methods: {},
 };
 </script>
