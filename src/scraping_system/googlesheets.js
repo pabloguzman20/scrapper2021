b43477121873a9a96915c72f196fb58cfec52b25
Hernan Castro Inzunza <hernan.castro@uabc.edu.mx>
2021-05-17 11:04:34 -0700

    Add funcionality about rewrite the spreadsheet

--- src/scraping_system/googlesheets.js
+++ src/scraping_system/googlesheets.js
@@ -4,6 +4,44 @@ const credentials = require('./json/credentials.json');
 // let googleId = '1-B3FpZPj6n673FQbLblQrZe2OLYkYGZobBXsNzQclO4';
 
 /**
+ * Devuelve las cabeceras del documento. 
+ * @returns string Devuelve las cabeceras del documento.
+ */
+const getHeaders = function () {
+    return [
+        'Campus',
+        'Unidad Academica',
+        'No. de programa Educativo',
+        'Programa Educativo',
+        'Plan de Estudios',
+        'Matrícula del alumno',
+        'Nombre del alumno',
+        'Estado',
+        'Clave del PVVC en Kardex',
+        'Nombre del PVVC en el kardex',
+        'ID del Proyecto en el SIFPVU',
+        'Nombre del PVVC en el SIFPVU',
+        'Creditos del proyecto',
+        'Fecha de inicio',
+        'Fecha de término',
+        'Clave de la Unidad de Aprendizaje 1',
+        'Nombre de la Unidad de Aprendizaje 1',
+        'Creditos de la Unidad de Apendizaje 1',
+        'Clave de la Unidad de Aprendizaje 2',
+        'Nombre de la Unidad de Aprendizaje 2',
+        'Creditos de la Unidad de Apendizaje 2',
+        'Clave de la Unidad de Aprendizaje 3',
+        'Nombre de la Unidad de Aprendizaje 3',
+        'Creditos de la Unidad de Apendizaje 3',
+        'Clave de la Unidad de Aprendizaje 4',
+        'Nombre de la Unidad de Aprendizaje 4',
+        'Creditos de la Unidad de Apendizaje 4',
+        'Nombre de la Empresa (unidad receptora)',
+        'Numero de empleado del profesor',
+        'Nombre del profesor, tutor o investigador' 
+    ];
+}
+/**
  * Recibe un arreglo con las claves de los PVVC y los agrega al
  * documento en Google Docs.
  * @param {*} values 
@@ -15,9 +53,13 @@ const addRowsGoogleSheet = async function (values, googleId) {
         await document.loadInfo();
 
         const sheet = document.sheetsByIndex[0];
+        await sheet.clear(); 
+        await sheet.resize({ rowCount: (values.length + 1) , columnCount: 30 });
+        await sheet.setHeaderRow(getHeaders());
         await sheet.addRows(values);
         return ['Se ha completado con exito la operación.'];
     } catch (error) {
+        console.log('Google Service error: ' + error);
         return (error.toString().includes('403')) ? 'El sistema no tiene permisos de editor en el documento.' : 'El url/googleId no fue encontrado.';
     }
 }
@@ -32,6 +74,7 @@ const authGoogleService = async function (googleId) {
         return (error.toString().includes('403')) ? 'El sistema no tiene permisos de editor en el documento.' : 'El url/googleId no fue encontrado.';
     }
 }
+
 
 module.exports = {
     addRowsGoogleSheet: addRowsGoogleSheet,
--- test/googlesheets_tester.test.js
+++ test/googlesheets_tester.test.js
@@ -5,9 +5,10 @@ test('Autenticación a Google Service con credenciales erroneas', async () => {
     expect(await gs.addRowsGoogleSheet(null, '1EdI9yDpm4veIRiNOkGwyNwez4C_3VqxFwvEJuL3W0Ck')).toMatch('El sistema no tiene permisos de editor en el documento.');
 });
 
-
-
-
 test('Autenticación a Google Service con googleid inexistente', async () => {
     expect(await gs.addRowsGoogleSheet(null, 'error')).toMatch('El url/googleId no fue encontrado.');
+});
+
+test('Autenticación a Google Service con credenciales correctas', async () => {
+    expect(await gs.authGoogleService('1-B3FpZPj6n673FQbLblQrZe2OLYkYGZobBXsNzQclO4')).toMatch('Vínculo correcto.');
 });
